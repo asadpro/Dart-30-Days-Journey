dbec0b364c721596398023c35f30d2fd1bdb4bec
asadpro <asadjani202@gmail.com>
2023-04-28 08:57:15 +0500

    object class uploading

--- dart_day_32_ObjectClass.dart
+++ dart_day_32_ObjectClass.dart
@@ -1,36 +1,44 @@
-void main() {
-  /*
-  All the classes are extending the object class which can be also called as base class e.g 
-  int
-  double
-  map 
-  list
-  set
-  bool 
-  String
-  num 
-  Iterable
-  these all are the subclass of object class. And you will be wonder why print statement can print any
-  value we put into it. That's because print statement takes object as an argument. If you hover mouse over it
-  then you see it clearly.
-  */
-  int x = 1;
-  print(x
-      .runtimeType); /*Press . notation after removing runtimeType property and you will see all the methods 
-  which are belongs to object class.
-*/
-  print('\n===================');
-  print(Car(
-      model: 2020,
-      tiers:
-          4)); //if you write this statement without converting it to String this will tell you it's instance of class Car
-}
+// import 'dart:html';
 
-class Car {
-  Car({required this.model, required this.tiers});
-  int tiers;
-  int model;
+// void main() {
+//   /*
+//   All the classes are extending the object class which can be also called as base class e.g 
+//   int
+//   double
+//   map 
+//   list
+//   set
+//   bool 
+//   String
+//   num 
+//   Iterable
+//   these all are the subclass of object class. And you will be wonder why print statement can print any
+//   value we put into it. That's because print statement takes object as an argument. If you hover mouse over it
+//   then you see it clearly.
+//   */
+//   int x = 1;
+//   print(x
+//       .runtimeType); /*Press . notation after removing runtimeType property and you will see all the methods 
+//   which are belongs to object class.
+// */
+//   print('\n===================');
+//   print(Car(
+//       model: 2020,
+//       tiers:
+//           4)); //if you write this statement without converting it to String this will tell you it's instance of class Car
+// }
 
-  @override
-  String toString() => 'Tyres are: $tiers\nModel is: $model';
-}
+// class Car {
+//   Car({required this.model, required this.tiers});
+//   int tiers;
+//   int model;
+
+//   @override
+//   String toString() => 'Tyres are: $tiers\nModel is: $model';
+// }
+
+
+//uncoment all of the above after commenting below code
+
+
+
